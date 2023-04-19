9ee78cafb0f54e3bcc5e106da13220d676f0c50d
Gayatri Satish Suryawanshi <89183954+Gayatri3108@users.noreply.github.com>
2023-04-19 20:41:44 +0530

    first commit

---
+++ daythree/daythree.js
@@ -0,0 +1,58 @@
+
+// alert("congragulation")
+// alert("hello");
+// alert(4+2);
+// var b=2;
+// b*=2;
+// alert(b)
+// let num=15;
+//   alert(num)
+//   const a=167;
+//   alert(a);
+//  a=122;
+//    alert(a);
+
+ //assignment operator
+// let a=13
+
+// a*=5;
+// alert(a)
+// //relation operator
+// var res=12>0;
+// alert(res)
+
+// //logical operator
+// let b;
+// alert(b>12 && b<59);
+
+// var x=13;
+// var y=12;
+// alert(x==y && x>y)
+
+// if(x>y){
+//    alert("gayatri")
+// }
+//    else
+//    {
+//     alert("suryawanshi")
+//    }
+
+//    //typeof "1"
+//    //alert(typeof(1));
+//    //alert(typeof(true));
+
+//   // let age=prompt("whats your age?",100)
+//    //alert("your age is"+age)
+
+   function myFunction() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "pic3.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(x);
+  }
+
+
+   
+   
